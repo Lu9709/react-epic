26d73018ff6ec3f68dceb3f26d9869ff4682294a
baizhe <919041098@qq.com>
2022-03-12 17:47:50 +0800

    init 引入styled

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {Suspense, lazy} from 'react'
+import {Routes, Route} from 'react-router-dom'
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Loading from "./components/Loading";
+
+const Home = lazy(() => import('./pages/Home'));
+const History = lazy(() => import('./pages/History'));
+const About = lazy(() => import('./pages/About'))
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <Header/>
+            <main>
+                <Suspense fallback={<Loading/>}>
+                    <Routes>
+                        <Route path='/' element={<Home/>} exact/>
+                        <Route path='/About' element={<About/>} exact/>
+                        <Route path='/History' element={<History/>} exact/>
+                    </Routes>
+                </Suspense>
+            </main>
+            <Footer/>
+        </>
+    );
 }
 
 export default App;
