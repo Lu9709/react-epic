3c415b94b07b7080572ff3bc01b8412e9e8b40c3
baizhe <919041098@qq.com>
2022-03-20 21:27:12 +0800

    完善Auth实现登录注册接口

---
+++ src/models/index.js
@@ -0,0 +1,34 @@
+import AV,{Query,User} from "leancloud-storage";
+
+AV.init({
+    appId: "FqKCj2VbsTgmnDK0mS7ne9DO-gzGzoHsz",
+    appKey: "Q23i6gbdRerQOw9C9HhgYTdr",
+    serverURL: "https://fqkcj2vb.lc-cn-n1-shared.com"
+});
+
+console.log('start...')
+
+const Auth = {
+    register(username,password){
+        let user = new User();
+        user.setUsername(username)
+        user.setPassword(password)
+        return new Promise((resolve,reject)=>{
+            user.signUp().then(loginedUser=> resolve(loginedUser),error => resolve(error))
+        })
+    },
+    login(username,password){
+        return new Promise((resolve,reject)=>{
+            User.logIn(username,password).then(loginedUser=> resolve(loginedUser),error=>reject(error))
+        })
+    },
+    logout(){
+        User.logOut()
+    },
+    getCurrentUser(){
+        return User.current();
+    }
+}
+
+export default {Auth};
+
