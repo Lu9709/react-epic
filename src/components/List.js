693925f2f236258c8e562f69bcab5f0aff784189
baizhe <919041098@qq.com>
2022-05-01 15:04:44 +0800

    上传历史全局状态管理

---
+++ src/components/List.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {observable} from "mobx";
+import {useStores} from "../stores";
+
+const Component = () => {
+  return (
+    <>
+      list...
+    </>
+  )
+}
+export default Component;
--- src/stores/index.js
+++ src/stores/index.js
@@ -2,16 +2,19 @@ import {createContext,useContext} from "react";
 import AuthStore from "./auth";
 import UserStore from "./user";
 import ImageStore from "./image";
+import HistoryStore from "./history";
 
 const context = createContext({
     AuthStore,
     UserStore,
-    ImageStore
+    ImageStore,
+    HistoryStore
 })
 
 window.store = {
     AuthStore,
     UserStore,
-    ImageStore
+    ImageStore,
+    HistoryStore
 }
 export const useStores = () => useContext(context)
